b35534b699c7400a89f7697b4f187e78245e87e8
Mario <mario@die-davids.de>
2019-02-23 21:13:53 +0100

    updated to 0.5.0

--- modules/web/src/de/diedavids/cuba/ceuesr/web/document/DocumentBrowse.java
+++ modules/web/src/de/diedavids/cuba/ceuesr/web/document/DocumentBrowse.java
@@ -1,16 +1,12 @@
 package de.diedavids.cuba.ceuesr.web.document;
 
-import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.core.global.MetadataTools;
 import com.haulmont.cuba.gui.ScreenBuilders;
 import com.haulmont.cuba.gui.UiComponents;
-import com.haulmont.cuba.gui.components.Component;
 import com.haulmont.cuba.gui.components.GroupTable;
-import com.haulmont.cuba.gui.components.LinkButton;
-import com.haulmont.cuba.gui.components.Table;
-import com.haulmont.cuba.gui.model.CollectionContainer;
 import com.haulmont.cuba.gui.screen.*;
 import de.diedavids.cuba.ceuesr.entity.Document;
+import de.diedavids.cuba.entitysoftreference.web.SoftReferenceInstanceNameTableColumnGenerator;
 
 import javax.inject.Inject;
 
@@ -34,24 +30,13 @@ public class DocumentBrowse extends StandardLookup<Document> {
 
     @Subscribe
     protected void onInit(InitEvent event) {
-        documentsTable.addGeneratedColumn("refersToInstanceName", new Table.ColumnGenerator<Document>() {
-            @Override
-            public Component generateCell(Document entity) {
-                LinkButton linkButton = uiComponents.create(LinkButton.class);
-
-                Entity refersTo = entity.getRefersTo();
-                linkButton.setCaption(metadataTools.getInstanceName(refersTo));
-
-                linkButton.addClickListener(clickEvent -> {
-                    screenBuilders.editor((Class<Entity>) refersTo.getClass(), DocumentBrowse.this)
-                            .editEntity(refersTo)
-                            .build().show();
-                });
-
-                return linkButton;
-            }
-        });
+        documentsTable.addGeneratedColumn("refersToInstanceName",
+                new SoftReferenceInstanceNameTableColumnGenerator(
+                        "refersTo",
+                        uiComponents,
+                        metadataTools,
+                        screenBuilders,
+                        this
+                        ));
     }
-    
-    
 }
--- modules/web/src/de/diedavids/cuba/ceuesr/web/document/DocumentEdit.java
+++ modules/web/src/de/diedavids/cuba/ceuesr/web/document/DocumentEdit.java
@@ -1,20 +1,17 @@
 package de.diedavids.cuba.ceuesr.web.document;
 
+import com.haulmont.cuba.core.entity.Entity;
 import com.haulmont.cuba.gui.ScreenBuilders;
-import com.haulmont.cuba.gui.Screens;
-import com.haulmont.cuba.gui.builders.LookupBuilder;
-import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import de.diedavids.cuba.ceuesr.entity.Customer;
 import de.diedavids.cuba.ceuesr.entity.Document;
 import de.diedavids.cuba.ceuesr.entity.Order;
+import de.diedavids.cuba.entitysoftreference.web.SoftReferenceFormFieldGenerator;
 
 import javax.inject.Inject;
-import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
-import java.util.function.Consumer;
 
 @UiController("ceuesr_Document.edit")
 @UiDescriptor("document-edit.xml")
@@ -28,11 +25,23 @@ public class DocumentEdit extends StandardEditor<Document> {
     @Inject
     protected ScreenBuilders screenBuilders;
 
+    @Inject
+    private Form form;
+
+    @Inject
+    private SoftReferenceFormFieldGenerator softReferenceFormFieldGenerator;
+
+
+    @Subscribe
+    protected void onInit(InitEvent event) {
+        softReferenceFormFieldGenerator.initSoftReferenceFormField(form, documentDc, "refersTo");
+    }
+
     @Subscribe("assignToCustomer")
     protected void onAssignToCustomer(Action.ActionPerformedEvent event) {
         screenBuilders.lookup(Customer.class, this).withSelectHandler(customers -> {
             Customer customer = new LinkedList<>(customers).getFirst();
-            documentDc.getItem().setRefersTo(customer);
+            setSoftReferenceValue(customer);
         }).build().show();
     }
 
@@ -40,10 +49,13 @@ public class DocumentEdit extends StandardEditor<Document> {
     protected void onAssignToOrder(Action.ActionPerformedEvent event) {
         screenBuilders.lookup(Order.class, this).withSelectHandler(orders -> {
             Order order = new LinkedList<>(orders).getFirst();
-            documentDc.getItem().setRefersTo(order);
+            setSoftReferenceValue(order);
         }).build().show();
     }
 
+    private void setSoftReferenceValue(Entity softReferenceValue) {
+        documentDc.getItem().setRefersTo(softReferenceValue);
+    }
     
     
 }
